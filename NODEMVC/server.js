b37268126b7e035e686b7e4a1b504b79f25f5267
Rajni Goyal <goyaljanvi900@gmail.com>
2026-05-07 21:48:51 +0530

    fix: cors  path error

--- NODEMVC/server.js
+++ NODEMVC/server.js
@@ -21,15 +21,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload());
 
-// CORS
 app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: false
+  origin: "https://stich-aura-1vwa.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-app.options(/.*/, cors());
+
 // ROUTES
 app.use("/user", userRouter);
 app.use("/customer", userRouterCus);
