ed355c8aa18fef9701fb2403892fede7a6e0bdbb
Rajni Goyal <goyaljanvi900@gmail.com>
2026-05-09 12:17:28 +0530

    gemini update

--- NODEMVC/config/genai.js
+++ NODEMVC/config/genai.js
@@ -1,12 +1,17 @@
+// const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+
+// const genAI = new GoogleGenerativeAI("AIzaSyDjVp2GX6Kjxn6wQWXa4LBSQLFSGpFAp2I");
+
+// const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+
+
+require("dotenv").config();
+
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-
-const genAI = new GoogleGenerativeAI("AIzaSyDjVp2GX6Kjxn6wQWXa4LBSQLFSGpFAp2I");
-
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
-
-
-
 
 //--------------------------------------------------------
 
